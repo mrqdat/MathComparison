e7d8d99ba8f359866b8718ca705d35414bdfcb2a
datnq35 <datnq35@fpt.com>
2024-11-25 22:51:39 +0700

    add swagger

--- MathComparison/Program.cs
+++ MathComparison/Program.cs
@@ -1,20 +1,31 @@
 using MathComparison.src.Application.Services;
 using MathComparison.src.Domain.Interfaces;
-using MathComparison.src.Infrastructure.Services;
 
 var builder = WebApplication.CreateSlimBuilder(args);
-//builder.Services.AddScoped<MathExpressionGenerator>();
-//builder.Services.AddScoped<MathComparisonService>();
-builder.Services.AddScoped<IMathExpressionService, MathExpressionGenerator>();
+builder.Services.AddScoped<IMathExpressionService, MathComparisonService>();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Math compare api",
+        Version = "v1",
+        Description = "API for comparing math expressions."
+    });
+});
 
 var app = builder.Build();
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
- 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Math compare api");
+             
+        c.RoutePrefix = "";
+    });
+}
 app.UseRouting();
 app.MapControllers();
 
